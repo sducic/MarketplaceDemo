d2a2e8c012219fab444b62442798596f5ce1498b
Stefan Ducic <59136829+sducic@users.noreply.github.com>
2025-06-13 18:38:54 +0200

    Add NavigationController, MainVC, HomeVC

--- MarketplaceDemo/UI/HomeVC.swift
+++ MarketplaceDemo/UI/HomeVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: MainVC {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ MarketplaceDemo/UI/MainVC.swift
@@ -0,0 +1,30 @@
+//
+//  MainVC.swift
+//  MarketplaceDemo
+//
+//  Created by Stefan Ducic on 13. 6. 2025..
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        overrideUserInterfaceStyle = .light
+        
+        setupNavigationTitle()
+    }
+    
+    func setupNavigationTitle()
+    {
+        let headerTitle = UILabel()
+        headerTitle.font = UIFont.boldSystemFont(ofSize: 18)
+        headerTitle.text = "Marketplace"
+        headerTitle.textColor = UIColor.black
+        navigationItem.titleView = headerTitle
+    }
+    
+
+}

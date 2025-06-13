2824ad1f039631d0d9e9f9b0e2e480c9e8d3334f
Stefan Ducic <59136829+sducic@users.noreply.github.com>
2025-06-13 23:55:01 +0200

    Implement UIColor+ extension

---
+++ MarketplaceDemo/Extensions/UIColor+.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+.swift
+//  MarketplaceDemo
+//
+//  Created by Stefan Ducic on 13. 6. 2025..
+//
+
+import Foundation
+import UIKit
+
+extension UIColor
+{
+    static var navigationTitleTextColor : UIColor { return UIColor(named:"navigationTitleTextColor")! }
+}
